223188ce8d32af61416eeb94ceedc4e86551d007
julie <juliana.chanel@gmail.com>
2025-03-15 16:44:27 -0700

    added readme

---
+++ Graph/README.md
@@ -0,0 +1,10 @@
+## Graph
+
+This program implements a basic graph with DFS (Depht First Search) and BFS (Breadth First Search) traversal
+
+## Program Description
+
+The test graph is weighted and directional and includes 5 vertices and 5 edges.\ <img title="Test Graph" alt="Test Graph Diagram" src="Graph\Graph_Diagram.png">
+
+Juliana Serrano\
+03/17/2025
--- Graph/obj/Debug/net8.0/Graph.AssemblyInfo.cs
+++ Graph/obj/Debug/net8.0/Graph.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b2c085c3a434cf5eddaa0b82e7896c6819a9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b845408fb84f2f491a98abed2b976bcfbd37f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
